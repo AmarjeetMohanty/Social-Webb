48599aa75414ec3d3cbd4b8dabace789f6abe75d
Amarjeet <amarmohanty06112003@gmail.com>
2023-06-13 17:34:51 +0530

    few changes

--- tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-const PORT = process.env.PORT || 6001
-// console.log("hello")
-// mongoose.connect(process.env.MONGO_URL,{
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-// }).then(()=>{
-//     app.listen(PORT,()=>console.log(`Server Port :${PORT}`));
-// }).catch((error)=>console.log(`${error} did not connect`));
