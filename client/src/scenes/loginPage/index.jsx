1b17bb5dc2bb869e385d5519d6c19c85f341ca21
Amarjeet <amarmohanty06112003@gmail.com>
2023-06-15 23:36:51 +0530

    loginpage

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,11 +1,11 @@
 import { Box, Typography,useTheme,useMediaQuery } from "@mui/material";
-
+import Form from "./Form"
 const LoginPage = () => {
     const theme = useTheme()
     const isNonMobileScreens = useMediaQuery("(min-width: 1000px")
 
     return <Box>
-        <Box width="100%" backgroungColor={theme.palette.background.alt} p="1rem 6%" >
+        <Box width="100%" backgroungColor={theme.palette.background.alt} p="1rem 6%" textAlign="center">
         <Typography
                 fontweight="bold"
                 fontSize="32px"
@@ -13,6 +13,12 @@ const LoginPage = () => {
                 >
                 Sociopedia
             </Typography>
+            <Box width={isNonMobileScreens ? "50%" : "93%"} p="2rem" m="2rem auto" borderRadius="1.5rem" backgroungColor={theme.palette.background.alt}>
+            <Typography fontWeight="500" variant="h5" sx={{mb: "1.5rem"}}>
+            Welcome to Sociopedia, the Social Media for Sociopaths!
+            </Typography>
+            <Form></Form>
+            </Box>
         </Box>
     </Box>
 };
